9adbc60eba29f0104210b2e40b62ee37d2fa6ae0
Priyansh Thakre <priyanshthakre2811@gmail.com>
2026-07-31 08:49:58 +0530

    Configure production CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
  cors: {
-  origin: "*",
+  origin: "https://code-sync-six-silk.vercel.app",
   methods: ["GET", "POST"],
 },
 });
