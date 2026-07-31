754a3c11fee8ea2e6816093060c5fde050af5b47
Priyansh Thakre <priyanshthakre2811@gmail.com>
2026-07-31 08:37:30 +0530

    Configure production socket connection

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
+ cors: {
+  origin: "*",
+  methods: ["GET", "POST"],
+},
 });
 
 // roomId -> array of socket ids
